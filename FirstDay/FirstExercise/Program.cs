4386ab154ced6b16ac016de8b9f078ba0f4b004e
Stefano Nucida <s.nucida@lendingsolution.it>
2022-12-10 16:13:20 +0100

    First Day

---
+++ FirstDay/FirstExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Diagnostics.Stopwatch stopwatch = new();
+string input = await File.ReadAllTextAsync("input.txt");  
+stopwatch.Start();
+var highest = 0;
+foreach(string elf in input.Split("\n\n"))
+{
+    int subSum = elf.Split('\n').Sum(s=> int.Parse(s));
+    if(subSum > highest) highest = subSum;
+}
+stopwatch.Stop();
+System.Console.WriteLine($"Execution Time: {stopwatch.ElapsedMilliseconds}ms");
+System.Console.WriteLine(highest);  
---
+++ FirstDay/SecondExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Diagnostics.Stopwatch stopwatch = new();
+var input = await File.ReadAllTextAsync("input.txt"); 
+PriorityQueue<int,int> q = new();
+stopwatch.Start();
+foreach(var cal in input.Split("\n\n"))
+{
+    int subSum = cal.Split("\n").Sum(s=> int.Parse(s)); 
+    if(q.Count<3)
+    {
+        q.Enqueue(subSum,subSum);
+    }
+    else
+    {
+        q.EnqueueDequeue(subSum,subSum);
+    } 
+}
+int totalValue = q.UnorderedItems.Sum(s => s.Element);
+stopwatch.Stop(); 
+System.Console.WriteLine($"Execution Time: {stopwatch.ElapsedMilliseconds}ms");
+System.Console.WriteLine(totalValue);
+
+ 
